cd3640e45afab1e5563ffdfb8bc4922596c450ce
Jitske-Leegwater <89977801+Jitske-Leegwater@users.noreply.github.com>
2021-10-08 18:29:18 +0200

    Add files via upload

---
+++ Opdracht eerste JavaScript/index.js
@@ -0,0 +1,13 @@
+console.log('Hello Winc Academy');
+let name = 'Jitske';
+console.log(name);
+console.log(4+4);
+console.log('4'+'4');
+console.log(4-4);
+console.log(4*4);
+console.log(4/4);
+console.log(4%4);
+let age = 32;
+console.log(typeof age);
+age = '32';
+console.log(typeof age);
