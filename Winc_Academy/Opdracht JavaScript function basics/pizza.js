c9876aa85bffc5c2251059bc48d81b914a432888
Jitske-Leegwater <89977801+Jitske-Leegwater@users.noreply.github.com>
2021-10-11 20:28:07 +0200

    Add files via upload

---
+++ Winc_Academy/Opdracht JavaScript function basics/pizza.js
@@ -0,0 +1,21 @@
+const makePizza = function () {
+    console.log('Make dough');
+    console.log('Spread dough in a circle');
+    console.log('Put sauce on the dough');
+    console.log('Put toppings on top of pizzadough');
+    console.log('Put pizza in oven for 10 minutes');
+};
+
+const makeSushi = function() {
+    console.log('Cook rice');
+    console.log('Spread rice in thin layer on top of seaweed');
+    console.log('Add filling');
+    console.log('Roll the seaweed tightly');
+    console.log('Cut the sushiroll in 8 pieces');
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
