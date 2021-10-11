6cbdcaa1689a95d3c901f2ae9070f9117b24420a
Jitske-Leegwater <89977801+Jitske-Leegwater@users.noreply.github.com>
2021-10-11 13:15:26 +0200

    Add files via upload

---
+++ Winc_Academy/Opdracht if else statements/index.js
@@ -0,0 +1,23 @@
+const age = 32;
+
+if (age >= 18 ) {
+    console.log("Welkom in de kroeg.");
+} else {
+    console.log("Helaas heb je nog niet de juiste leeftijd. BLijf aub buiten.");
+}
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log("Welkom op de ladiesnight");
+} else {
+    console.log("vandaag is enkel voor de dames. Morgen verwelkomen we je graag");
+}
+
+const driverStatus = "bob";
+
+if (driverStatus == "bob") {
+    console.log("Je bent bob en mag rijden");
+} else {
+    console.log("je bent geen bob en mag dus niet rijden");
+}
