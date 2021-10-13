af32e62744662d8bbc152ab58a237137354dfe10
Jitske-Leegwater <89977801+Jitske-Leegwater@users.noreply.github.com>
2021-10-13 21:47:28 +0200

    Add files via upload

---
+++ Winc_Academy/Opdracht JavaScript loops/multiplication-tables.js
@@ -0,0 +1,10 @@
+for (i = 1; i <= 10; i++) {
+    console.log(i + ' x 9 = ' + i * 9);
+};
+
+for (y = 1; y <= 10; y++) {
+    for (x = 1; x <= 10; x++) {
+        console.log(y + ' x ' + x + ' = ' + y * x);
+    };
+    console.log(' ');
+};
